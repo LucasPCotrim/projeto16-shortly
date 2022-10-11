0c458438b3e4fa4b8e5cba6b4fae3c664b14bf48
LucasPCotrim <lucaspcotrim@gmail.com>
2022-10-11 17:57:00 -0300

    feat: Implemented urlSchema

--- src/schemas/urlSchema.js
+++ src/schemas/urlSchema.js
@@ -1,1 +1,15 @@
 import joi from 'joi';
+const urlPattern = new RegExp(
+  '((http|https)://)(www.)?[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)'
+);
+
+const urlSchema = joi.object({
+  url: joi.string().regex(urlPattern).required().messages({
+    'string.base': `"url" should be of type 'string'`,
+    'string.empty': `"url" cannot be an empty field`,
+    'any.required': `"url" is a required field`,
+    'any.regex': `"url" must be an URL`,
+  }),
+});
+
+export default urlSchema;

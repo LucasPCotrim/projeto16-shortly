44cf05e2f54a9fce28305baf3445c8286fd25a87
LucasPCotrim <lucaspcotrim@gmail.com>
2022-10-11 04:00:19 -0300

    feat: Implemented table creation script

--- src/database/createTables.sql
+++ src/database/createTables.sql
@@ -1,11 +1,23 @@
 CREATE TABLE "users" (
-
+  "id" SERIAL PRIMARY KEY,
+  "name" TEXT NOT NULL,
+  "email" TEXT NOT NULL UNIQUE,
+  "password" TEXT NOT NULL,
+  "createdAt" TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE "sessions" (
-
+  "id" SERIAL PRIMARY KEY,
+  "token" TEXT NOT NULL UNIQUE,
+  "userId" INTEGER NOT NULL REFERENCES "users"("id"),
+  "createdAt" TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE "urls" (
-
+  "id" SERIAL PRIMARY KEY,
+  "url" TEXT NOT NULL,
+  "shortUrl" TEXT NOT NULL UNIQUE,
+  "visitCount" INTEGER NOT NULL DEFAULT 0 CHECK("visitCount" >= 0),
+  "userId" INTEGER REFERENCES "users"("id"),
+  "createdAt" TIMESTAMP NOT NULL DEFAULT NOW()
 );
